9f08ec615157624af6d71157305ccf6d30fd05d7
Paola Ferrario <paola.ferrario@gmail.com>
2019-04-25 16:16:28 +0200

    Add test for mc writer.

--- antea/io/mc_io_test.py
+++ antea/io/mc_io_test.py
@@ -2,8 +2,34 @@ import os
 import numpy  as np
 import tables as tb
 
+from . mc_io import mc_sns_response_writer
+
 from . mc_io import read_mcsns_response
 from . mc_io import read_mcTOFsns_response
+
+from invisible_cities.reco.tbl_functions import get_mc_info
+
+
+def test_mc_sns_response_writer_reset(output_tmpdir):
+    filein  = os.environ['ANTEADIR'] + '/testdata/full_ring_test.pet.h5'
+    fileout = os.path.join(output_tmpdir, "test_mc_sns_response_writer_reset.h5")
+
+    with tb.open_file(filein) as h5in:
+        with tb.open_file(fileout, 'w') as h5out:
+
+            mc_writer  = mc_sns_response_writer(h5out)
+            events_in  = np.unique(h5in.root.MC.extents[:]['evt_number'])
+
+            sns_dict = {1000: 1, 1001: 2, 1002: 3}
+            wvfs     = {e: sns_dict for e in events_in}
+
+            assert mc_writer.last_row              == 0
+
+            mc_writer(get_mc_info(h5in), wvfs, events_in[0])
+            assert mc_writer.last_row              == 1
+
+            mc_writer.reset()
+            assert mc_writer.last_row              == 0
 
 
 def test_read_sensor_response():
